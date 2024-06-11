e8ef331cecccb8185a281272e538d8455fabe5f5
Muhammad Zarif Abd Raman <mabd-ram@u82z04s01.42kl.edu.my>
2024-06-11 15:45:50 +0800

    ft_memmove.c completed

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,22 +1,35 @@
-void *ft_memmove(void *dest, const void *src, size_t n)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabd-ram <mabd-ram@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/11 13:02:54 by mabd-ram          #+#    #+#             */
+/*   Updated: 2024/06/11 15:40:54 by mabd-ram         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-  char *dest_ptr;
-  const char *src_ptr;
-  int i;
+	char		*dest_ptr;
+	const char	*src_ptr;
+	int			i;
 
-  dest_ptr = (char *)dest;
-  src_ptr = (const char *)src;
-  i = n - 1;
-
-  if (dest_ptr > src_ptr && dest_ptr < src_ptr + n)
-  {
-    while (i >= 0)    
-    {
-      dest_ptr[i] = src_ptr[i];
-      i--;
-    }
-  }
-  else
-    ft_memcpy(dest, src, n);
-  return (dest);
+	dest_ptr = (char *)dest;
+	src_ptr = (const char *)src;
+	i = n - 1;
+	if (dest_ptr > src_ptr && dest_ptr < src_ptr + n)
+	{
+		while (i >= 0)
+		{
+			dest_ptr[i] = src_ptr[i];
+			i--;
+		}
+	}
+	else
+	{
+		ft_memcpy(dest, src, n);
+	}
+	return (dest);
 }
