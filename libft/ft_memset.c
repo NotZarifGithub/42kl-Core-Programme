c4fef144db26ea470ee49ff051eafecbbbbc02ba
Muhammad Zarif Abd Raman <mabd-ram@u82z04s01.42kl.edu.my>
2024-06-11 15:46:09 +0800

    ft_memset.c completed

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -1,13 +1,25 @@
-void *ft_memset(void *str, int c, size_t len)
-{
-  unsigned char *p;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabd-ram <mabd-ram@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/11 13:03:12 by mabd-ram          #+#    #+#             */
+/*   Updated: 2024/06/11 15:42:50 by mabd-ram         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-  p = (unsigned char *)str;
-  while (len > 0)  
-  {
-    *p = (unsigned char)c;
-    p++;
-    len--;
-  }
-  return (str)
+void	*ft_memset(void *str, int c, size_t len)
+{
+	unsigned char	*p;
+
+	p = (unsigned char *)str;
+	while (len > 0)
+	{
+		*p = (unsigned char)c;
+		p++;
+		len--;
+	}
+	return (str);
 }
