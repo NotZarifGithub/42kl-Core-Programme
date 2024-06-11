d4214d17cda1ad7a0cbe8b91f5da15aeacb0b66c
Muhammad Zarif Abd Raman <mabd-ram@u82z04s01.42kl.edu.my>
2024-06-11 18:34:36 +0800

    ft_atoi.c completed

---
+++ libft/ft_atoi.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabd-ram <mabd-ram@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/11 16:40:11 by mabd-ram          #+#    #+#             */
+/*   Updated: 2024/06/11 18:34:20 by mabd-ram         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	atoi(const char *str)
+{
+	int	sign;
+	int	result;
+
+	sign = 1;
+	result = 0;
+	while (*str == ' ' || *str == '\t' || *str == '\n')
+		str++;
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	else if (*str == '+')
+	{
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}		
+	return (sign * result);
+}
