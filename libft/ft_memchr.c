8299eede8888fd26771adb89e267990f0809199e
Muhammad Zarif Abd Raman <mabd-ram@u82z04s01.42kl.edu.my>
2024-06-11 15:45:13 +0800

    ft_memchr.c completed

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -1,15 +1,27 @@
-void *memchr(const void *str, int c, size_t n)
-{
-  unsigned char *str_ptr;
-  int i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabd-ram <mabd-ram@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/11 13:02:25 by mabd-ram          #+#    #+#             */
+/*   Updated: 2024/06/11 15:30:57 by mabd-ram         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-  str_ptr = (unsigned char *)str;
-  i = 0;
-  while (n > i)
-  {
-    if (str_ptr[i] == (unsigned char)c)
-      return (&str_ptr[i]);
-    i++;
-  }
-  return (NULL);
+void	*memchr(const void *str, int cl, size_t n)
+{
+	unsigned char	*str_ptr;
+	int				i;
+
+	str_ptr = (unsigned char *)str;
+	i = 0;
+	while (n > i)
+	{
+		if (str_ptr[i] == (unsigned char)c)
+			return (&str_ptr[i]);
+		i++;
+	}
+	return (NULL);
 }
