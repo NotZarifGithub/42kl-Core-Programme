78e0ba8b3be15903c0508c631d4b67f001ef30ff
Muhammad Zarif Abd Raman <mabd-ram@u80z06s03.42kl.edu.my>
2024-06-12 16:13:26 +0800

    ft_strdup.c completed

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,8 +6,19 @@
 /*   By: mabd-ram <mabd-ram@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 18:53:33 by mabd-ram          #+#    #+#             */
-/*   Updated: 2024/06/11 18:53:35 by mabd-ram         ###   ########.fr       */
+/*   Updated: 2024/06/12 16:13:06 by mabd-ram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strdup(const char *str)
+{
+	size_t len;
+	char *dup;
 
+	len = strlen(str);
+	dup = malloc(len + 1);
+	if (dup == NULL)
+		return (NULL);
+	memcpy(dup, str, len + 1);
+	return (dup);
+}
