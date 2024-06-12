8fe8be1efcb5af900e5acc455dd05e27aeb4d0d1
Muhammad Zarif Abd Raman <mabd-ram@u80z06s03.42kl.edu.my>
2024-06-12 18:26:04 +0800

    ft_substr.c completed

---
+++ libft/ft_substr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabd-ram <mabd-ram@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/12 16:15:58 by mabd-ram          #+#    #+#             */
+/*   Updated: 2024/06/12 18:25:29 by mabd-ram         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char *substring;
+	
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		len = 0;
+	substring = malloc(len + 1);
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
+
+	memcpy(substring, s + start, len);
+	substring[len] = '\0';
+	return (substring);
+}
