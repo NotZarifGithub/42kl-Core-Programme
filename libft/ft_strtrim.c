068be970df92a4b0ac48eba5f84ffc9197627cae
Muhammad Zarif Abd Raman <mabd-ram@u80z06s03.42kl.edu.my>
2024-06-13 13:47:00 +0800

    ft_strtrim.c completed

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -1,18 +1,30 @@
-char *ft_strtrim(char const *s1, char const *set)
-{
-    char *trimmed_str;
-    int start;
-    int end;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabd-ram <mabd-ram@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/13 12:31:19 by mabd-ram          #+#    #+#             */
+/*   Updated: 2024/06/13 12:38:08 by mabd-ram         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    start = 0;
-    end = ft_strlen(s1) - 1;
-    while (start <= end && ft_strchr(set, s1[start]) != NULL)
-      start++;    
-    while (start >= end && ft_strchr(set, s1[end]) != NULL)
-      end--;
-    trimmed_str = malloc((end - start + 2) * sizeof(char)) 
-    if (trimmed_str == NULL)
-        return (NULL);
-    ft_strlcpy(trimmed_str, s1 + start, end - start + 2);
-    return (trimmed_str);
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	trimmed_str;
+	int		start;
+	int		end;
+
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (start <= end && ft_strchr(set, s1[start]) != NULL)
+		start++;
+	while (start >= end && ft_strchr(set, s1[end]) != NULL)
+		end--;
+	trimmed_str = malloc((end - start + 2) * sizeof(char));
+	if (trimmed_str == NULL)
+		return (NULL);
+	ft_strlcpy(trimmed_str, s1 + start, end - start + 2);
+	return (trimmed_str);
 }
