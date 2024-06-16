ddf9036d8db9064f7ac85d7c0d83d098b8be6a49
root <root@Fujitsu-Fugaku>
2024-06-16 16:46:05 +0800

    ft_bzero fix

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_bzero(void *s, size_t n)
+void	ft_bzero(void *str, size_t n)
 {
 	ft_memset(s, 0, n);
 }
