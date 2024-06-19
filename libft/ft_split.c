f4f85276550bee523f4ac1c85000c258bba70b66
root <root@Fujitsu-Fugaku>
2024-06-20 02:24:47 +0800

    ft_split completed

---
+++ libft/ft_split.c
@@ -0,0 +1,68 @@
+static size_t	ft_countword(char const *s, char c)
+{
+	size_t	count;
+
+	if (!*s)
+		return (0);
+	count = 0;
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		if (*s)
+			count++;
+		while (*s != c && *s)
+			s++;
+	}
+	return (count);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**lst;
+	size_t	word_len;
+	int		i;
+
+	lst = (char **)malloc((ft_countword(s, c) + 1) * sizeof(char *));
+	if (!s || !lst)
+		return (0);
+	i = 0;
+	while (*s)
+	{
+		while (*s == c && *s)
+			s++;
+		if (*s)
+		{
+			if (!ft_strchr(s, c))
+				word_len = ft_strlen(s);
+			else
+				word_len = ft_strchr(s, c) - s;
+			lst[i++] = ft_substr(s, 0, word_len);
+			s += word_len;
+		}
+	}
+	lst[i] = NULL;
+	return (lst);
+}
+
+/*int main()
+{
+    char **words;
+    char str[] = "I love Kanye West";
+
+    words = ft_split(str, ' '); // space character as delimiter
+
+    for (int i = 0; words[i]; i++)
+    {
+        printf("%s\n", words[i]);
+    }
+
+    // Don't forget to free the memory!
+    for (int i = 0; words[i]; i++)
+    {
+        free(words[i]);
+    }
+    free(words);
+
+    return 0;
+}*/
