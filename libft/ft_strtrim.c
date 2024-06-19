bf1737413e4fc194d9d7f4b3e0fcc1b82f481856
Muhammad Zarif Abd Raman <mabd-ram@u81z08s01.42kl.edu.my>
2024-06-19 17:55:47 +0800

    ft_strtrim fix

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,13 +6,13 @@
 /*   By: mabd-ram <mabd-ram@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 12:31:19 by mabd-ram          #+#    #+#             */
-/*   Updated: 2024/06/13 12:38:08 by mabd-ram         ###   ########.fr       */
+/*   Updated: 2024/06/19 17:54:53 by mabd-ram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(const char *s1, const char *set)
 {
-	char	trimmed_str;
+	char	*trimmed_str;
 	int		start;
 	int		end;
 
@@ -20,7 +20,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	end = ft_strlen(s1) - 1;
 	while (start <= end && ft_strchr(set, s1[start]) != NULL)
 		start++;
-	while (start >= end && ft_strchr(set, s1[end]) != NULL)
+	while (start <= end && ft_strchr(set, s1[end]) != NULL)
 		end--;
 	trimmed_str = malloc((end - start + 2) * sizeof(char));
 	if (trimmed_str == NULL)
@@ -28,3 +28,17 @@ char	*ft_strtrim(char const *s1, char const *set)
 	ft_strlcpy(trimmed_str, s1 + start, end - start + 2);
 	return (trimmed_str);
 }
+
+/*int main(void)
+{
+	const char *str1;
+	const char *set;
+	char *trimmed_str;
+
+	str1 = "!!!I love Kanye West!!!!";
+	set = "!";
+	trimmed_str = ft_strtrim(str1, set);
+	printf("String: %s\n", str1);
+	printf("Trimmed string: %s\n", trimmed_str);
+	return (0);	
+}*/
