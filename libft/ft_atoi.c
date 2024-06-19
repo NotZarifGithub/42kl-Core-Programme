cfa179ef8f353b12dd6b53c40848cc0dc71f4a20
Muhammad Zarif Abd Raman <mabd-ram@u81z08s01.42kl.edu.my>
2024-06-19 14:40:04 +0800

    ft_atoi fix

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,11 +6,11 @@
 /*   By: mabd-ram <mabd-ram@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 16:40:11 by mabd-ram          #+#    #+#             */
-/*   Updated: 2024/06/11 18:35:05 by mabd-ram         ###   ########.fr       */
+/*   Updated: 2024/06/19 14:39:33 by mabd-ram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	atoi(const char *str)
+int	ft_atoi(const char *str)
 {
 	int	sign;
 	int	result;
@@ -35,3 +35,18 @@ int	atoi(const char *str)
 	}
 	return (sign * result);
 }
+
+/*int main(void)
+{
+	const char *str1, *str2, *str3, *str4;
+
+	str1 = "1241721";
+	str2 = "-34142";
+	str3 = "+235235";
+	str4 = "  23452";
+	printf("%d\n", ft_atoi(str1));
+	printf("%d\n", ft_atoi(str2));
+	printf("%d\n", ft_atoi(str3));
+	printf("%d\n", ft_atoi(str4));
+	return (0);
+}*/
