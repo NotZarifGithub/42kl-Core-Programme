9072d832180303746eafd9c25e7a78e1993054bd
Muhammad Zarif Abd Raman <mabd-ram@u81z08s01.42kl.edu.my>
2024-06-19 16:11:01 +0800

    ft_substr fix

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,11 +6,11 @@
 /*   By: mabd-ram <mabd-ram@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 16:15:58 by mabd-ram          #+#    #+#             */
-/*   Updated: 2024/06/12 18:34:57 by mabd-ram         ###   ########.fr       */
+/*   Updated: 2024/06/19 16:09:40 by mabd-ram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
 	char	*substring;
 
@@ -21,7 +21,23 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	substring = malloc(len + 1);
 	if (len > ft_strlen(s) - start)
 		len = ft_strlen(s) - start;
-	memcpy(substring, s + start, len);
+	ft_memcpy(substring, s + start, len);
 	substring[len] = '\0';
 	return (substring);
 }
+
+/*int main(void)
+{
+	const char *str1;
+	unsigned int start;
+	size_t len;
+	char *substr;
+
+	str1 = "I love Kanye West";
+	start = 7;
+	len = 10;
+	substr = ft_substr(str1, start, len);
+	printf("String: %s\n", str1);
+	printf("Substring: %s\n", substr);
+	return (0);
+}*/
