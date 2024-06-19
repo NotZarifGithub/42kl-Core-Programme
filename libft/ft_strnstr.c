0ae89a3ba2b44729d1af4d515dc96f0d1b7b7399
Muhammad Zarif Abd Raman <mabd-ram@u81z08s01.42kl.edu.my>
2024-06-19 14:08:04 +0800

    ft_strnstr fix

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,11 +6,11 @@
 /*   By: mabd-ram <mabd-ram@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 15:42:53 by mabd-ram          #+#    #+#             */
-/*   Updated: 2024/06/06 17:04:12 by mabd-ram         ###   ########.fr       */
+/*   Updated: 2024/06/19 14:03:04 by mabd-ram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*strnstr(const char *hay, const char *needle, size_t n)
+char	*ft_strnstr(const char *hay, const char *needle, size_t n)
 {
 	int	i;
 	int	j;
@@ -24,8 +24,24 @@ char	*strnstr(const char *hay, const char *needle, size_t n)
 		while (hay[i + j] == needle[j] && needle[j] != '\0')
 			j++;
 		if (needle[j] == '\0')
-			return ((char *)(&hay[i - j]));
+			return ((char *)(&hay[i]));
 		i++;
 	}
 	return (NULL);
+}
+
+int main(void)
+{
+	const char *hay1;
+	const char *needle1;
+	size_t len;
+	char *result1;
+
+	hay1 = "I love Kanye West.";
+	needle1 = "Kanye";
+	len = strlen(hay1);
+	result1 = ft_strnstr(hay1, needle1, len);
+	if (result1 != NULL)
+		printf("Needle found: %s\n", result1);
+	return (0);
 }
